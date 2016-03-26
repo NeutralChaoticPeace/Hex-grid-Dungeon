af1b1af61f0b8741b04ebb4ba216172c04ff6c5c
Zachary <zachary.allen@live.com>
2016-03-26 15:07:01 -0700

    started room generator

---
+++ HexGridDungeon/HexGridDungeon/HexGridDungeon/WorldGeneration/RoomGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HexGridDungeon.WorldGeneration
+{
+    public class RoomGenerator
+    {
+        private int MinRoomSize;
+        private int MaxroomSize;
+
+        public RoomGenerator(int _MinRoomSize, int _MaxRoomSize)
+        {
+            MinRoomSize = _MinRoomSize;
+            MaxroomSize = _MaxRoomSize;
+        }
+
+
+        public HexGrid GenerateNewRoom()
+        {
+            int rand = Rand.GetInstance().Next(0, 100);
+
+            if (rand <= 50)
+                return BuildSimpleRoom();
+            else if (50 < rand && rand <= 100)
+                return BuildWaterRoom();
+            else
+                return BuildSimpleRoom();
+        } 
+
+
+        private HexGrid BuildSimpleRoom()
+        {
+            return null;
+        }
+
+
+        private HexGrid BuildWaterRoom()
+        {
+            return null;
+        }
+
+    }
+}
