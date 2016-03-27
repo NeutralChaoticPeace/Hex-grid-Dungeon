3789eef3e31b630c694c36d5434b859cf6ce271b
Tim Vierow <timmyviers@hotmail.com>
2016-03-27 01:34:33 -0700

    Changed game.cs so clicking mouse now changes map by 1 iteration (rather than spinning through many).

--- HexGridDungeon/HexGridDungeon/HexGridDungeon/Game1.cs
+++ HexGridDungeon/HexGridDungeon/HexGridDungeon/Game1.cs
@@ -86,10 +86,15 @@ namespace HexGridDungeon
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
+			bool pressed = false;
+
 			// Allows the game to exit
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
 				this.Exit();
-			if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+			if (Keyboard.GetState().GetPressedKeys().Contains(Keys.Enter)) { pressed = true; }
+			while (Mouse.GetState().LeftButton == ButtonState.Pressed) { pressed = true; }
+
+			if (pressed)
 				this.Initialize();
 
 			// TODO: Add your update logic here
