6b61879aaafdca24ebb89bb9742454d9a0dab2af
Zachary <zachary.allen@live.com>
2016-03-27 00:58:16 -0700

    created new tile options and sprites to go with them

---
+++ HexGridDungeon/HexGridDungeon/HexGridDungeon/Tiles/TileTypes/WallTypes/StoneWall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HexGridDungeon.Tiles.TileTypes.WallTypes
+{
+    public class StoneWall : Wall
+    {
+        public StoneWall()
+        {
+            spriteID = "StoneWallHexSprite";
+        }
+    }
+}
